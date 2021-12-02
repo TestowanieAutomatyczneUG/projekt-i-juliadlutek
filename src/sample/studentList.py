f4f49865cc064a923efc848182425580d77f12ee
Julia Dlutek <jdlutek987@gmail.com>
2021-12-02 18:59:37 +0100

    Dodane metody init i addStudent do klasy StudentList

--- src/sample/studentList.py
+++ src/sample/studentList.py
@@ -1,4 +1,11 @@
+from src.sample.student import Student
+
 
 class StudentList:
-   def __init__(self):
-      return
+    def __init__(self):
+        self.students = []
+
+    def addStudent(self, name, surname):
+        student = Student(name, surname)
+        self.students.append(student)
+        return "Dodano nowego ucznia: " + str(name) + " " + str(surname)
--- tests/test_assertpy_student_list.py
+++ tests/test_assertpy_student_list.py
@@ -2,13 +2,13 @@ import unittest
 from assertpy import *
 from src.sample.studentList import StudentList
 
+
 class StudentListAssertpyTest(unittest.TestCase):
     def setUp(self):
         self.temp = StudentList()
 
     def test_add_student(self):
         assert_that(self.temp.addStudent("Maria", "Kowalska"), "Dodano nowego ucznia: Maria Kowalska")
-
 
     def tearDown(self):
         self.temp = None
