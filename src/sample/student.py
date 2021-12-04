a14bdade90013ba930a3f0e5f0863a22958151ba
Julia Dlutek <jdlutek987@gmail.com>
2021-12-04 12:23:32 +0100

    Testy do metody add_student w StudentList uwzględniające przypadki z niepoprawnymi argumentami

--- src/sample/student.py
+++ src/sample/student.py
@@ -1,7 +1,4 @@
-
 class Student:
-   def __init__(self, name, surname):
-      self.name = name
-      self.surname = surname
-
-
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
--- tests/test_assertpy_student_list.py
+++ tests/test_assertpy_student_list.py
@@ -10,6 +10,90 @@ class StudentListAssertpyTest(unittest.TestCase):
     def test_add_student(self):
         assert_that(self.temp.addStudent("Maria", "Kowalska"), "Dodano nowego ucznia: Maria Kowalska")
 
+    def test_add_student_number_name(self):
+        assert_that(
+            self.temp.add_student) \
+            .raises(ValueError) \
+            .when_called_with(65, "Kowalska") \
+            .contains("Student's name must be string!")
+
+    def test_add_student_number_surname(self):
+        assert_that(
+            self.temp.add_student) \
+            .raises(ValueError) \
+            .when_called_with("Maria", 0) \
+            .contains("Student's surname must be string!")
+
+    def test_add_student_empty_string_name(self):
+        assert_that(
+            self.temp.add_student) \
+            .raises(ValueError) \
+            .when_called_with("", "Kowalska") \
+            .contains("Student's name must be string!")
+
+    def test_add_student_empty_string_surname(self):
+        assert_that(
+            self.temp.add_student) \
+            .raises(ValueError) \
+            .when_called_with("Maria", "") \
+            .contains("Student's surname must be string!")
+
+    def test_add_student_array_name(self):
+        assert_that(
+            self.temp.add_student) \
+            .raises(ValueError) \
+            .when_called_with(["Maria"], "Kowalska") \
+            .contains("Student's name must be string!")
+
+    def test_add_student_array_surname(self):
+        assert_that(
+            self.temp.add_student) \
+            .raises(ValueError) \
+            .when_called_with("Maria", ["Kowalska"]) \
+            .contains("Student's surname must be string!")
+
+    def test_add_student_boolean_name(self):
+        assert_that(
+            self.temp.add_student) \
+            .raises(ValueError) \
+            .when_called_with(True, "Kowalska") \
+            .contains("Student's name must be string!")
+
+    def test_add_student_boolean_surname(self):
+        assert_that(
+            self.temp.add_student) \
+            .raises(ValueError) \
+            .when_called_with("Maria", False) \
+            .contains("Student's surname must be string!")
+
+    def test_add_student_none_name(self):
+        assert_that(
+            self.temp.add_student) \
+            .raises(ValueError) \
+            .when_called_with(None, "Kowalska") \
+            .contains("Student's name must be string!")
+
+    def test_add_student_none_surname(self):
+        assert_that(
+            self.temp.add_student) \
+            .raises(ValueError) \
+            .when_called_with("Maria", None) \
+            .contains("Student's surname must be string!")
+
+    def test_add_student_float_name(self):
+        assert_that(
+            self.temp.add_student) \
+            .raises(ValueError) \
+            .when_called_with(65.267, "Kowalska") \
+            .contains("Student's name must be string!")
+
+    def test_add_student_float_surname(self):
+        assert_that(
+            self.temp.add_student) \
+            .raises(ValueError) \
+            .when_called_with("Maria", 0.1) \
+            .contains("Student's surname must be string!")
+
     def tearDown(self):
         self.temp = None
 
