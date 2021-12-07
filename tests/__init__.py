0a1bbfd9ffe76b0159a2e179ce36272ac6b68f1f
Julia Dlutek <jdlutek987@gmail.com>
2021-12-07 15:39:49 +0100

    Zmiany w strukturze projektu

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,5 @@
-# the inclusion of the tests module is not meant to offer best practices for
-# testing in general, but rather to support the `find_packages` example in
-# setup.py that excludes installing the "tests" package
+
+from .test_assertpy_student_list import *
+from .test_assertpy_student import *
+from .test_pyhamcrest_student_list import *
+from .test_pyhamcrest_student import *
