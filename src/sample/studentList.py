f448c04fd539b06faa9ffa06cead20aec80e0bb5
Julia Dlutek <jdlutek987@gmail.com>
2021-12-07 15:59:07 +0100

    Testy do wyświetlania listy uczniów

--- src/sample/studentList.py
+++ src/sample/studentList.py
@@ -9,3 +9,6 @@ class StudentList:
         student = Student(name, surname)
         self.students.append(student)
         return "Dodano nowego ucznia: " + str(name) + " " + str(surname)
+
+    def getAllStudents(self):
+        return
--- tests/test_pyhamcrest_student_list.py
+++ tests/test_pyhamcrest_student_list.py
@@ -5,7 +5,18 @@ from src.sample.studentList import StudentList
 
 class StudentListPyHamcrestTest(unittest.TestCase):
     def setUp(self):
-        return
+        self.temp = StudentList()
+
+    def test_get_empty_student_list(self):
+        assert_that(self.temp.getAllStudents(), equal_to(''))
+
+    def test_get_student_list(self):
+        self.temp.addStudent("Maria", "Kowalska")
+        self.temp.addStudent("Jan", "Nowak")
+        self.temp.addStudent("Anna", "Nowak")
+        assert_that(
+            self.temp.getAllStudents(),
+            equal_to_ignoring_whitespace("1. Maria Kowalska 2. Jan Nowak 3. Anna Nowak"))
 
     def tearDown(self):
         self.temp = None
