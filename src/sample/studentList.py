9af3da82da436642079bcdc341cc38711822a7cc
Julia Dlutek <jdlutek987@gmail.com>
2021-12-07 16:06:46 +0100

    Funkcja wyświetlająca listę uczniów

--- src/sample/studentList.py
+++ src/sample/studentList.py
@@ -11,4 +11,9 @@ class StudentList:
         return "Dodano nowego ucznia: " + str(name) + " " + str(surname)
 
     def getAllStudents(self):
-        return
+        result = ""
+        i = 1
+        for student in self.students:
+            result += str(i) + ". " + str(student.name) + " " + str(student.surname) + "\n"
+            i += 1
+        return result
