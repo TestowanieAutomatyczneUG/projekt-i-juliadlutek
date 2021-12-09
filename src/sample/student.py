2ff6db1ab4887083141827f8253d210b9d3a0775
Julia Dlutek <jdlutek987@gmail.com>
2021-12-09 15:57:54 +0100

    Testy w doctest do edycji imienia ucznia

--- src/sample/student.py
+++ src/sample/student.py
@@ -1,8 +1,43 @@
 class Student:
     def __init__(self, name, surname):
         if type(name) != str or len(name) == 0:
-            raise ValueError("Student's name must be string!")
+            raise ValueError("Imię ucznia musi być typu string!")
         elif type(surname) != str or len(surname) == 0:
-            raise ValueError("Student's surname must be string!")
+            raise ValueError("Nazwisko ucznia musi być typu string!")
         self.name = name
         self.surname = surname
+
+    def editStudentName(self, name):
+        """Changes the student's name
+        >>> s.editStudentName("Marysia")
+        'Zmieniono imię ucznia na Marysia!'
+        >>> s.editStudentName("")
+        Traceback (most recent call last):
+          ...
+        ValueError: Imię ucznia nie może być puste!
+        >>> s.editStudentName(10)
+        Traceback (most recent call last):
+          ...
+        ValueError: Imię ucznia musi być typu string!
+        >>> s.editStudentName(True)
+        Traceback (most recent call last):
+          ...
+        ValueError: Imię ucznia musi być typu string!
+        >>> s.editStudentName(None)
+        Traceback (most recent call last):
+          ...
+        ValueError: Imię ucznia musi być typu string!
+        >>> s.editStudentName(["Ala"])
+        Traceback (most recent call last):
+          ...
+        ValueError: Imię ucznia musi być typu string!
+        >>> s.editStudentName(-2.3)
+        Traceback (most recent call last):
+          ...
+        ValueError: Imię ucznia musi być typu string!
+        """
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod(extraglobs={'s': Student("Maria", "Kowalska")})
