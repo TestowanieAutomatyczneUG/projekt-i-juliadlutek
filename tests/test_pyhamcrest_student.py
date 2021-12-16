7153427543a670ba0c50ee368814b0e4a1bf3767
Julia Dlutek <jdlutek987@gmail.com>
2021-12-16 19:01:45 +0100

    Testy do funkcji usuwającej przedmiot

--- tests/test_pyhamcrest_student.py
+++ tests/test_pyhamcrest_student.py
@@ -1,11 +1,36 @@
 import unittest
 from hamcrest import *
-from src.sample.studentList import StudentList
+from src.sample.student import Student
 
 
 class StudentPyHamcrestTest(unittest.TestCase):
     def setUp(self):
-        return
+        self.temp = Student("Jan", "Nowak", 10)
+        self.temp.addStudentLecture("Matematyka")
+
+    def test_delete_student_lecture(self):
+        assert_that(self.temp.deleteStudentLecture("Matematyka"), equal_to("Usunięto przedmiot - Matematyka!"))
+
+    def test_delete_not_existing_student_lecture(self):
+        assert_that(calling(self.temp.deleteStudentLecture).with_args("Angielski"), raises(Exception))
+
+    def test_delete_student_lecture_empty_str(self):
+        assert_that(calling(self.temp.deleteStudentLecture).with_args(""), raises(ValueError))
+
+    def test_delete_student_lecture_int(self):
+        assert_that(calling(self.temp.deleteStudentLecture).with_args(123), raises(ValueError))
+
+    def test_delete_student_lecture_float(self):
+        assert_that(calling(self.temp.deleteStudentLecture).with_args(0.1), raises(ValueError))
+
+    def test_delete_student_lecture_empty_bool(self):
+        assert_that(calling(self.temp.deleteStudentLecture).with_args(False), raises(ValueError))
+
+    def test_delete_student_lecture_none(self):
+        assert_that(calling(self.temp.deleteStudentLecture).with_args(None), raises(ValueError))
+
+    def test_delete_student_lecture_empty_array(self):
+        assert_that(calling(self.temp.deleteStudentLecture).with_args([]), raises(ValueError))
 
     def tearDown(self):
         self.temp = None
