02edef570c4d1a2f87ce2b5b6326677a72d2d213
Julia Dlutek <jdlutek987@gmail.com>
2021-12-18 11:08:29 +0100

    Własny matcher w pyhamcrest i testy do funkcji obliczającej średnią ze wszystkich przedmiotów

--- tests/test_pyhamcrest_student.py
+++ tests/test_pyhamcrest_student.py
@@ -1,17 +1,33 @@
 import unittest
 from hamcrest import *
 from src.sample.student import Student
+from hamcrest.core.base_matcher import BaseMatcher
+
+
+class IsFloat(BaseMatcher):
+    def _matches(self, item):
+        if type(item) != float:
+            return False
+        return item
+
+    def describe_to(self, description):
+        description.append_text("Result is float")
+
+
+def is_float():
+    return IsFloat()
 
 
 class StudentPyHamcrestTest(unittest.TestCase):
     def setUp(self):
         self.temp = Student("Jan", "Nowak", 10)
-        self.temp.addStudentLecture("Matematyka")
 
     def test_delete_student_lecture(self):
+        self.temp.addStudentLecture("Matematyka")
         assert_that(self.temp.deleteStudentLecture("Matematyka"), equal_to("Usunięto przedmiot - Matematyka!"))
 
     def test_delete_not_existing_student_lecture(self):
+        self.temp.addStudentLecture("Matematyka")
         assert_that(calling(self.temp.deleteStudentLecture).with_args("Angielski"), raises(Exception))
 
     def test_delete_student_lecture_empty_str(self):
@@ -31,6 +47,36 @@ class StudentPyHamcrestTest(unittest.TestCase):
 
     def test_delete_student_lecture_empty_array(self):
         assert_that(calling(self.temp.deleteStudentLecture).with_args([]), raises(ValueError))
+
+    def test_get_student_final_average_correct(self):
+        self.temp.addStudentLecture("Matematyka")
+        self.temp.addStudentLecture("Angielski")
+        self.temp.addStudentGrade("Angielski", 4)
+        self.temp.addStudentGrade("Angielski", 3)
+        self.temp.addStudentGrade("Matematyka", 5)
+        self.temp.addStudentGrade("Matematyka", 4)
+        self.temp.addStudentGrade("Matematyka", 4)
+        assert_that(self.temp.getStudentFinalAverage(), close_to(4, 0.1))
+
+    def test_get_student_final_average_type(self):
+        self.temp.addStudentLecture("Matematyka")
+        self.temp.addStudentLecture("Angielski")
+        self.temp.addStudentGrade("Angielski", 4)
+        self.temp.addStudentGrade("Angielski", 3)
+        self.temp.addStudentGrade("Matematyka", 5)
+        self.temp.addStudentGrade("Matematyka", 4)
+        assert_that(self.temp.getStudentFinalAverage(), is_float())
+
+    def test_get_student_final_average_any_lectures(self):
+        assert_that(calling(self.temp.getStudentFinalAverage).with_args(), raises(Exception))
+
+    def test_get_student_final_average_lecture_without_grades(self):
+        self.temp.addStudentLecture("Matematyka")
+        self.temp.addStudentLecture("Angielski")
+        self.temp.addStudentGrade("Angielski", 4)
+        self.temp.addStudentGrade("Angielski", 3)
+        assert_that(self.temp.getStudentFinalAverage(), greater_than(3))
+
 
     def tearDown(self):
         self.temp = None
